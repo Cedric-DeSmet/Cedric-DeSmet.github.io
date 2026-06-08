68b95b7e4fa0b24867f84bd95784ba8e2671e69a
Cedric-DeSmet <unenlightened690@gmail.com>
2026-06-09 00:54:15 +0300

    Remove YouTube links from Featured Projects

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -17,7 +17,6 @@ const portfolioProjects = [
       { title: "Improved site speed by 50%" },
       { title: "Increased mobile traffic by 35%" },
     ],
-    link: "https://youtu.be/4k7IdSLxh6w",
     image: darkSaasLandingPage,
   },
   {
@@ -29,7 +28,6 @@ const portfolioProjects = [
       { title: "Expanded customer reach by 35%" },
       { title: "Increased brand awareness by 15%" },
     ],
-    link: "https://youtu.be/7hi5zwO75yc",
     image: lightSaasLandingPage,
   },
   {
@@ -41,7 +39,6 @@ const portfolioProjects = [
       { title: "Improved site speed by 50%" },
       { title: "Increased mobile traffic by 35%" },
     ],
-    link: "https://youtu.be/Z7I5uSRHMHg",
     image: aiStartupLandingPage,
   },
 ];
@@ -84,16 +81,6 @@ export const ProjectsSection = () => {
                       </li>
                     ))}
                   </ul>
-                  <a
-                    href={project.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <button className="bg-white text-gray-950 h-12 w-full md:w-auto px-6 rounded-xl font-semibold inline-flex items-center justify-center gap-2 mt-8 mb-4 hover:bg-white/90 transition">
-                      <span>Visit Live Site</span>
-                      <ArrowUpRight className="size-4" />
-                    </button>
-                  </a>
                 </div>
                 <div className="relative">
                   <Image
